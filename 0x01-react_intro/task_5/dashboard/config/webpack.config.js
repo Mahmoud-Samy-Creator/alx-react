92ce3555b22e2ee4242af4e4b6c1456b237390c4
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-30 16:19:13 +0300

    webpack config

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,43 +2,41 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: '../src/index.js',
+    entry: path.join(__dirname, "../src/index.js"),
+
     output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+        path:path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js'
     },
-    mode: 'development',
+
     devtool: 'inline-source-map',
-    devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "../dist/index.html"),
+        }),
+    ],
+
     module: {
         rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
-                            disable: true,
-                        },
-                    },
-                ],
-            },
-        ],
+            ]
+        },
+
+    devServer: {
+    port: 3000,
     },
-    resolve: {
-        extentions: ['.js', '.jsx']
-    },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: '../dist/index.html',
-            }),
-        ],
 };
