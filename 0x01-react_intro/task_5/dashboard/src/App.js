808bf61585e459ee2eaf417cd3194930281a63e2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-30 16:15:57 +0300

    App component

---
+++ 0x01-react_intro/task_5/dashboard/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React with Webpack!</h1>
+      <h1>Hello, React with Webpack!</h1>
+    </div>
+  );
+};
+
+export default App;
