88118321075a567ef0e2c3a0d7c1a0d6bffeafb2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-30 16:39:18 +0300

    webpack config

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -39,25 +39,10 @@ module.exports = {
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65,
-                            },
-                            optipng: {
-                                enabled: true,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4,
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75,
-                            },
-                        },
-                    },
+                            bypassOnDebug: true,
+                            disable: true,
+                        }
+                    }
                 ],
             },
         ],
@@ -67,3 +52,4 @@ module.exports = {
         port: 3000,
     },
 };
+
