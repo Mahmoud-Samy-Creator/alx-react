6e9d44e7f7e80abafd06521f90f7f4d5628b3487
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-30 13:20:15 +0300

    Making the task normally from zero

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: '../src/index.js',
     output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -35,7 +35,7 @@ module.exports = {
 },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: '../dist/index.html',
         }),
     ],
 };
