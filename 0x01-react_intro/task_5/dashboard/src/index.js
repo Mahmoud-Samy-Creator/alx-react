8e321342e5601954b6ecb3ca693ff700569a368d
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-30 16:15:46 +0300

    Entry point

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello words')
+// src/index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(<App />);
