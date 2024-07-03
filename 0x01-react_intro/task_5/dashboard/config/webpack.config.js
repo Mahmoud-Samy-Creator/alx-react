4b451cc24c1360b772a3ffb91f79d4c264b2852c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-07-04 02:19:51 +0300

    bundling and organizing

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,42 +1,38 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: '../src/index.js',
     entry: path.join(__dirname, "../src/index.js"),
-
     output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-        path:path.resolve(__dirname, '../dist'),
-        filename: 'bundle.js'
+        path:path.resolve(__dirname, "../dist"),
+        filename: "bundle.js"
     },
+
     mode: 'development',
 
     devtool: 'inline-source-map',
-    devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-    },
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "../dist/index.html"),
+        template: path.join(__dirname, "../public/index.html"),
         }),
     ],
 
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                test: /\.?js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+            },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|jpg|gif)$/i,
@@ -48,22 +44,15 @@ module.exports = {
                             disable: true,
                         },
                     },
-                ],
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
         ],
-    },
-    resolve: {
-        extentions: ['.js', '.jsx']
-        },
 
+    },
     devServer: {
     port: 3000,
     },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: '../dist/index.html',
-            }),
-        ],
 };
