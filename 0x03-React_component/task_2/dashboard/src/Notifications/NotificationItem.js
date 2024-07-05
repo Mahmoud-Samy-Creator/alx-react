1caede983a8ea2f9a8ee0fbfbbc9fe3fb78dc88a
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-07-05 17:15:02 +0300

    task [2]

--- 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types'
 class NotificationItem extends Component {
     render() {
         const {type, html, value, markAsRead, id} = this.props;
-        if (value) return <li data={type} onClick = {(id) => markAsRead(id)}>{value}</li>;
-        return <li data={type} dangerouslySetInnerHTML={html} onClick = {(id) => markAsRead(id)}></li>
+        if (value) return <li data={type} onClick = {() => markAsRead(id)}>{value}</li>;
+        return <li data={type} dangerouslySetInnerHTML={html} onClick = {() => markAsRead(id)}></li>
     }
 }
 
--- 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/Notifications.js
@@ -23,7 +23,7 @@ class Notifications extends Component {
 			notificationsExists = <NotificationItem type='default' value='No new notification for now'/>
 		} else {
 			notificationsExists = listNotifications.map(({ id, html, type, value }) =>
-				<NotificationItem key={id} type={type} html={html} value = {value} markAsRead = {() => {this.markAsRead(id)}}/>
+				<NotificationItem key={id} type={type} value={value} html={html} markAsRead={this.markAsRead} />
 			)
 		}
 
